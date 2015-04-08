0caeb3ce3abb222b32887a6bfa17e80fc72007dc
khan08 <hkang413@gmail.com>
2015-04-08 15:09:18 -0400

    test

--- src/checkData.py
+++ src/checkData.py
@@ -3,4 +3,4 @@ import pandas as pd
 games = pd.read_pickle('c:/django projects/nbaScraping/data/games')
 print('games loaded')
 futureGames = pd.read_pickle('c:/django projects/nbaScraping/data/futureGames')
-#print('futureGames loaded')
+print('futureGames loaded')
